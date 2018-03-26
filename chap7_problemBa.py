26baa7a2a0ebd318d7f5dd0c7e8dca3d502b1724
bklimko <33734816+bklimko@users.noreply.github.com>
2018-03-26 15:27:39 -0500

    updated chapter 7 problem A, worked on B parts a and b

--- chap7_problemBa.py
+++ chap7_problemBa.py
@@ -2,33 +2,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
-# advect - Program to solve the advection equation
-# using the various hyperbolic PDE schemes
-
+# wave - Program to solve the wave equation
+# Benjamin Klimko, PHYS 416, Spring 2018
 #* Select numerical parameters (time step, grid spacing, etc.).
-method = int(input('Choose a numerical method: (1)FTCS, (2)Lax, (3)Lax-Wendroff, (4)Upwind: '))
+
 N = int(input('Enter number of grid points: '))
 L = 1.     # System size
-h = L/N    # Grid spacing
+h = L/(N-1)    # Grid spacing
 c = 1.      # Wave speed
 print('Time for wave to move one grid spacing is ',h/c)
 tau = float(input('Enter time step: '))
-coeff = -c*tau/(2.*h)  # Coefficient used by all schemes
-coefflw = 2*coeff**2    # Coefficient used by L-W scheme
+coeff = c**2*tau**2/(h**2)  # Coefficient
 print('Wave circles system in %6.0f steps'%(L/(c*tau)))
 nStep = int(input('Enter number of steps: '))
 #* Set initial and boundary conditions.
 sigma = 0.1              # Width of the Gaussian pulse
 k_wave = np.pi/sigma        # Wave number of the cosine
-x = (np.arange(0,N)+1./2.)*h - L/2  # Coordinates of grid points
+x = (np.arange(0,N)-1.)*h - L/2  # Coordinates of grid points
 # Initial condition is a Gaussian-cosine pulse
 a = np.cos(k_wave*x) * np.exp(-x**2/(2*sigma**2))
-# Use periodic boundary conditions
+a_new = np.zeros(len(a))
+a_old = np.copy(a)
+# Use Dirichlet boundary conditions
 # could also use np.roll here
-ip = np.arange(0,N)+1
-ip[N-1] = 0 # ip = i+1 with periodic b.c.
-im = np.arange(0,N)-1
-im[0] = N-1 # im = i-1 with periodic b.c.
+ip = np.arange(1,N-1)+1
+
+ip[0] = 0
+ip[N-3] = 0 # ip = i+1 with Dirichlet b.c. 
+im = np.arange(1,N-1)-1
+# im[0] = 0 # im = i-1 with Dirichlet b.c.
+im[N-3] = 0
+
 #* Initialize plotting variables.
 iplot = 0          # Plot counter
 aplot = np.copy(a)  # Record the initial state
@@ -37,16 +41,16 @@ nplots = 50        # Desired number of plots
 plotStep = max(1, np.floor(nStep/nplots)) # Number of steps between plots
 #* Loop over desired number of steps.
 for iStep in range(1,nStep+1):  ## MAIN LOOP ##
-	#* Compute new values of wave amplitude using FTCS,
-	#  Lax or Lax-Wendroff method.
-	if( method == 1 ):      ### FTCS method ###
-		a[0:N] = a[0:N] + coeff*(a[ip]-a[im])
-	elif( method == 2 ):  ### Lax method ###
-		a[0:N] = .5*(a[ip]+a[im]) + coeff*(a[ip]-a[im])
-	elif(method==3):                  ### Lax-Wendroff method ###
-		a[0:N] = a[0:N] + coeff*(a[ip]-a[im]) + coefflw*(a[ip]+a[im]-2*a[0:N])
-	elif(method==4):                  ### Upwind method ###
-		a[0:N] = a[0:N] + 2.*coeff*(a[0:N]-a[im])
+	#* Compute new values of wave amplitude 
+
+	a_new[1:N-1] = (2*a[1:N-1] - a_old[1:N-1] + coeff*(a[ip] - 2*a[1:N-1] + a[im]))
+	a_new[0] = 0
+	a_new[N-1] = 0
+
+	#store A^(n-1)
+	a_old = np.copy(a)
+	a = np.copy(a_new)
+	
 	#* Periodically record a(t) for plotting.
 	if( (iStep%plotStep) < 1 and iStep >0):  # Every plotStep steps record
 		iplot = iplot+1
